ffeb3c2d00cf2439a9baa90e47f73238da8bb73d
David Ochoa Resendiz <37809222+LordSox@users.noreply.github.com>
2021-12-06 23:06:47 +0000

    Change something

--- bash.sh
+++ bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 g++ -std=c++11 -c makedata3.cpp -o makedata3.o &&\
-  g++ -std=c++11 -c simpledatagenerator.cpp -o simpledatagenerator.o &&\
+  g++ -std=c++11 -c simpledatagenerator.h -o simpledatagenerator.o &&\
   g++ makedata3.o simpledatagenerator.o -o makedata3 &&\
   gnuplot -e " set term png enhauced linewidth 2 fontscale 1.75; set output 'plot3.png';plot 'database.dat' 2 l ls 1 title 'base', 'dataingh.dat' w l ls 2 title 'inherited'"
